be9c992a62ec1d8e78273653e95eb964020ab4f1
Snick-P <4eji41@gmail.com>
2023-07-27 20:24:03 +0300

    add allure reporting

---
+++ src/test/java/helpers/CustomAllureListener.java
@@ -0,0 +1,16 @@
+package helpers;
+
+import io.qameta.allure.restassured.AllureRestAssured;
+
+public class CustomAllureListener {
+    private static final AllureRestAssured FILTER = new AllureRestAssured();
+
+//    Файлы .ftl взяты из репозитория Allure Framework
+//    https://github.com/allure-framework/allure-java/blob/master/allure-attachments/src/main/resources/tpl/
+
+    public static AllureRestAssured withCustomTemplates(){
+        FILTER.setRequestTemplate("http-request.ftl");
+        FILTER.setResponseTemplate("http-response.ftl");
+        return FILTER;
+    }
+}
--- src/test/java/test/Login.java
+++ src/test/java/test/Login.java
@@ -1,16 +1,20 @@
 package test;
 
+import helpers.CustomAllureListener;
 import io.restassured.http.ContentType;
 import models.LoginBodyModel;
 import models.LoginResponseModel;
 import org.junit.jupiter.api.Test;
 
+import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 
 public class Login {
 
     public static String baseUrl = "http://localhost:8080";
+
 
 
     @Test
@@ -19,8 +23,10 @@ public class Login {
         loginBody.setPassword("password");
         loginBody.setUsernameOrEmail("leanne");
 
-        LoginResponseModel loginResponse =
+
+        LoginResponseModel loginResponse = step ("Get authorization data", ()->
         given()
+                .filter(CustomAllureListener.withCustomTemplates())
                 .log().uri()
                 .contentType(ContentType.JSON)
                 .body(loginBody)
@@ -29,10 +35,14 @@ public class Login {
                 .then()
                 .log().all()
                 .statusCode(200)
-                .extract().as(LoginResponseModel.class);
+                .extract().as(LoginResponseModel.class));
 
-        assertThat(loginResponse.getTokenType()).isEqualTo("Bearer");
-        assertThat(loginResponse.getAccessToken()).contains("eyJhbGciOiJIUzUxMiJ9");
+        step("Verify authorization token", ()->
+        assertThat(loginResponse.getAccessToken()).contains("eyJhbGciOiJIUzUxMiJ9"));
+
+        step ("Verify authorization token type",()->
+            assertThat(loginResponse.getTokenType()).isEqualTo("Bearer"));
+
 
 
     }
